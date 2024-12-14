8cf7712c416aed63ea0ca28ba3ccf040f9211456
unknown <kottugoroshko232@gmail.com>
2024-12-14 19:12:15 +0200

    finished task

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,12 @@ class Post(models.Model):
     
     # def published_recently(self):
     #     return self.published_date
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from blog.models import Post, Author
+from blog.models import Post, Author, Comment
 # Create your views here.
 def show_all_posts(request):
     posts = Post.objects.all()
+    authors = Author.objects.all()
     context = {
+        "author_list":authors,
         "post_list":posts,
     }
     return render(
@@ -14,8 +16,11 @@ def show_all_posts(request):
 
 def post_details_by_id(request, post_id):
     post = Post.objects.get(id=post_id)
+    comments = Comment.objects.all().filter(post = post)
     context = {
+
         "post":post,
+        "comment":comments,
     }
     return render(
         request=request,
@@ -26,7 +31,7 @@ def show_all_author_posts(request, author_id):
     author = Author.objects.get(id=author_id)
     posts = Post.objects.all().filter(author = author)
     context = {
-        
+        "author":author.name,
         "posts":posts,
     }
     return render(
